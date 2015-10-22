daf4ada1a3d3619d75459b7aa69418106853b2d5
Fang Chai <fangbai0330@hotmail.com>
2015-10-22 15:29:54 +0800

    untested&unrefined rough expand code

--- ssel_expand.cpp
+++ ssel_expand.cpp
@@ -15,7 +15,7 @@ void less_expand(LessBlock& currenBlock,bool firstInvoke=false)
     for(auto &elem : currenBlock){
         switch(elem.type){
             case LessElementType::DEF:
-                build_def_token((PDef)elem.data,tokenize);
+                build_def_token((PDef)elem.data,tokenizer);
                 break;
             case LessElementType::NORMAL_SELECTOR:
                 build_normalselector_token((PSelector)elem.data,tokenizer );
@@ -36,24 +36,32 @@ void less_expand(LessBlock& currenBlock,bool firstInvoke=false)
     //Add the tokenmap to token list.
     G_tokenList.push_back(tokenizer);
     //Second time scan.
-    for(auto &elem : currenBlock)
+    for(auto elem =currenBlock.begin();elem!=currenBlock.end();++elem)
     {
-        switch(elem.type){
+        switch(elem->type){
             case LessElementType::DEF:
                 break;
             case LessElementType::MIXIN:
                 auto block=expand_mixin((PMixin)elem.data);
+                LessSelector scoped_block={"",vector<LessParam>(),block};
+                elem=currenBlock.erase(elem);
+                currenBlock.insert(elem,scoped_block);
                 break;
             case LessElementType::BLOCK_COMMENT:
                 break;
             case LessElementType::CSS_RULE:
                 break;
             case LessElementType::NORMAL_SELECTOR:
+                less_expand((PSelector)elem->data->selector_body);
                 break;
             case LessElementType::PARAMETRIC_SELECTOR:
+                less_expand((PSelector)elem->data->selector_body);
                 break;
         }
     }
+    //Throw away token list never to be use.
+    //Make sure the token list is always at the same depth as our analyze goes.
+    G_tokenList.pop_back();
 }
 LessBlock expand_mixin(PMixin mixin)
 {
@@ -61,20 +69,19 @@ LessBlock expand_mixin(PMixin mixin)
     //Use a parallel set list to determine whether a name is in the token list.
     //Will improve the performance .
     if(mixin->params.empty())
-        for(auto iter=G_tokenList.rbegin();iter!=G_tokenList.rend();++iter){
-            for(auto iter2=(*iter1).rbegin();iter2!=(*iter1).rend();++iter2){
-                if(iter2->find(make_pair(mixin->name,LessElementType::NORMAL_SELECTOR))!=map::end||
-                   iter2->find(make_pair(mixin->name,LessElementType::PARAMETRIC_SELECTOR))!=map::end){
                     //Handling default parametric selector.
-                    if(iter2->at(make_pair(mixin->name,LessElementType::PARAMETRIC_SELECTOR)){
-                        auto selector = PSelector(
-                        iter2->at(make_pair(mixin->name,LessElementType::PARAMETRIC_SELECTOR)));
-                        auto body=seletor->selector_body;
-                        auto params=seletor->params;
+                    //Double call
+                    auto ptr_norm_selector=(PSelector)find_double_call_element(mixin->name,LessElementType::NORMAL_SELECTOR);
+                    auto ptr_para_selector=(PSelector)find_double_call_element(mixin->name,LessElementType::PARAMETRIC_SELECTOR);
+                    LessBlock merged_body;
+                    if(ptr_para_selector){
+                        auto body=ptr_para_seletor->selector_body;
+                        auto params=ptr_para_seletor->params;
                         for(auto &elem : params){
                             LessDef to_ins({elem.name,elem.expression});
                             body.insert(body.begin(),to_ins);
                         }
+                        /*
                         //Handling recursive calls.
                         for(auto iter3=body.begin();iter3!=body.end();++iter3){
                             if(iter3->type==LessElementType::MIXIN){
@@ -84,21 +91,14 @@ LessBlock expand_mixin(PMixin mixin)
                                 body.insert(iter3,block);
                             }
                         }
-                      return body;
+                        */
+                        merged_body.insert(merged_body.end(),body.begin(),body.end());
                     }
-                    else{
-                        auto body=(LessSelector(iter2->at(make_pair(mixin->name,LessElementType::NORMAL_SELECTOR)))->selector_body;
-                        //Handling recursive calls.
-                        for(auto iter3=body.begin();iter3!=body.end();++iter3){
-                            if(iter3->type==LessElementType::MIXIN){
-                                auto block = expand_mixin((PMixin)iter3->data);
-                                iter3=body.erase(iter3);
-                                LessSelector scoped_block={"",vector<LessParam>(),block};
-                                body.insert(iter3,block);
-                            }
-                        }
-                        return body;
+                    if(ptr_norm_selector){
+                        auto body=(LessSelector)ptr_norm_selector->selector_body;
+                        merged_body.insert(merged_body.end(),body.begin(),body.end());
                     }
+                    return merged_body;
                 }
                 else {
                 //TODO:
@@ -110,8 +110,33 @@ LessBlock expand_mixin(PMixin mixin)
     else{
         //Handling non-default parametric calls.
         //Check if a valid call first.
-
+        LessBlock merged_body;
+        auto ptr_norm_selector=(PSelector)find_double_call_element(mixin->name,LessElementType::NORMAL_SELECTOR);
+        auto ptr_para_selector=(PSelector)find_double_call_element(mixin->name,LessElementType::PARAMETRIC_SELECTOR);
+        if(ptr_para_seletor||ptr_para_selector->params.size()==mixin->params.size()){
+            LessBlock body=ptr_para_selector->selector_body;
+            for(int i=0;i<mixin->params.size();++i){
+                LessDef converted_Param({ptr_para_selector->params[i].name,mixin->params[i].expression});
+                body.insert(body.begin(),converted_Param);
+            }
+            merged_body.insert(merged_body.end(),body.begin(),body.end());
+        }
+        if(ptr_norm_selector){
+            auto body = ptr_norm_selector->selector_body;
+            merged_body.insert(merged_body.end(),body.begin(),body.end());
+        }
+        return merged_body;
     }
+}
+void *find_double_call_element(string elem_name,LessElementType elem_type)
+{
+    auto index=make_pair(elem_name,elem_type);
+    for(auto iter=G_tokenList.rbegin();iter!=G_tokenList.rend();++iter){
+        if(iter->find(index)!=map::end){
+            return iter->at(index);
+        }
+    }
+    return nullptr;
 }
 void build_normalselector_token(PSelector  nselector,TokenMap &tokenmap)
 {
